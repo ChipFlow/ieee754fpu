f5540e005ec6030e6fe16036431178ca2d174c31
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2019-03-29 12:43:26 +0000

    remove unneeded imports

--- src/add/fpcommon/corrections.py
+++ src/add/fpcommon/corrections.py
@@ -2,19 +2,9 @@
 # Copyright (C) Jonathan P Dawson 2013
 # 2013-12-12
 
-from nmigen import Module, Signal, Cat, Mux, Array, Const
-from nmigen.lib.coding import PriorityEncoder
+from nmigen import Module
 from nmigen.cli import main, verilog
-from math import log
-
-from fpbase import FPNumIn, FPNumOut, FPOp, Overflow, FPBase, FPNumBase
-from fpbase import MultiShiftRMerge, Trigger
-from singlepipe import (ControlBase, StageChain, UnbufferedPipeline,
-                        PassThroughStage)
-from multipipe import CombMuxOutPipe
-from multipipe import PriorityCombMuxInPipe
-
-from fpbase import FPState, FPID
+from fpbase import FPState
 from fpcommon.roundz import FPRoundData
 
 
