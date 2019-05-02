24239d9f0f2f4c2e0e74c220594e4462a8371e35
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2019-05-02 15:15:43 +0100

    move fpbase.py

--- src/ieee754/fpadd/test/test_add.py
+++ src/ieee754/fpadd/test/test_add.py
@@ -3,9 +3,10 @@ from operator import add
 from nmigen import Module, Signal
 from nmigen.compat.sim import run_simulation
 
-from nmigen_add_experiment import FPADD
+from ieee754.fpadd.nmigen_add_experiment import FPADD
 
-from unit_test_single import (get_mantissa, get_exponent, get_sign, is_nan,
+from iee754.fpcommon.unit_test_single import (get_mantissa, get_exponent,
+                                get_sign, is_nan,
                                 is_inf, is_pos_inf, is_neg_inf,
                                 match, get_rs_case, check_rs_case, run_test,
                                 run_edge_cases, run_corner_cases)
