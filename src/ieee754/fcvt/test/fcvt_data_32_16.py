d44b843ee14185e1526e00bc5430b78aecec5c1e
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2019-07-08 13:59:47 +0100

    add fcvt regression tests

---
+++ src/ieee754/fcvt/test/fcvt_data_32_16.py
@@ -0,0 +1,17 @@
+def regressions():
+    yield 0x40900000
+    yield 0x94607b66
+    yield 0x889cd8c
+    yield 0xe98646d7
+    yield 0x3340f2a7
+    yield 0xfff13f05
+    yield 0x453eb000
+    yield 0x3a05de50
+    yield 0xc27ff989
+    yield 0x41689000
+    yield 0xbbc0edec
+    yield 0x2EDBE6FF
+    yield 0x358637BD
+    yield 0x3340f2a7
+    yield 0x33D6BF95
+
