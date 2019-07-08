6f0924705ac4651213190318596e235be55bde81
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2019-07-08 14:29:24 +0100

    add fp cvt 64-16 test

---
+++ src/ieee754/fcvt/test/test_fcvt_pipe_64_16.py
@@ -0,0 +1,21 @@
+""" test of FPCVTMuxInOut
+"""
+
+from ieee754.fcvt.pipeline import (FPCVTMuxInOut,)
+from ieee754.fpcommon.test.case_gen import run_pipe_fp
+from ieee754.fpcommon.test import unit_test_single
+from ieee754.fcvt.test.fcvt_data_64_16 import regressions
+
+from sfpy import Float64, Float16
+
+def fcvt_16(x):
+    return Float16(x)
+
+def test_pipe_fp64_16():
+    dut = FPCVTMuxInOut(64, 16, 4)
+    run_pipe_fp(dut, 64, "add", unit_test_single, Float64,
+                regressions, fcvt_16, 10, True)
+
+if __name__ == '__main__':
+    test_pipe_fp64_16()
+
