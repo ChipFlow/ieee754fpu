3b0fcab584df5e1b1e5c8a52265cb631fa6149a2
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2019-07-14 12:50:55 +0100

    run loop of 1000 round test_fpmul_pipe.py

--- src/ieee754/fpmul/test/test_fpmul_pipe.py
+++ src/ieee754/fpmul/test/test_fpmul_pipe.py
@@ -9,24 +9,18 @@ from operator import mul
 
 def test_pipe_fp16():
     dut = FPMULMuxInOut(16, 4)
-    runfp(dut, 16, "test_fpmul_pipe_fp16", Float16, mul)
+    runfp(dut, 16, "test_fpmul_pipe_fp16", Float16, mul, n_vals=50)
 
 def test_pipe_fp32():
     dut = FPMULMuxInOut(32, 4)
-    runfp(dut, 32, "test_fpmul_pipe_fp32", Float32, mul)
+    runfp(dut, 32, "test_fpmul_pipe_fp32", Float32, mul, n_vals=50)
 
 def test_pipe_fp64():
     dut = FPMULMuxInOut(64, 4)
-    runfp(dut, 64, "test_fpmul_pipe_fp64", Float64, mul)
+    runfp(dut, 64, "test_fpmul_pipe_fp64", Float64, mul, n_vals=50)
 
 if __name__ == '__main__':
-    # XXX BUG: 0xe7bb 0x81ce 0x2afa
-    # XXX BUG: 0xe225 0x8181 0x249f -> 0x249e
-    # send 1 1 0x201 0x4901 0xd04 recv 1 0xd03 expected 0xd04
-    test_pipe_fp16()
-    test_pipe_fp32()
-
-    # XXX BUG 0x4e1b1e94ef100 0x42e1b4e00b65f134 0x2e59bf1d2667c4d  ->
-    #         recv 0x2e59bf1d2667c4c expected 0x2e59bf1d2667c4d
-
-    test_pipe_fp64()
+    for i in range(1000):
+        test_pipe_fp16()
+        test_pipe_fp32()
+        test_pipe_fp64()
