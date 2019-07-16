64ec4421dff6f723665f4200a6d031286df4f02a
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2019-07-16 17:44:48 +0100

    increase test range, add loop

--- src/ieee754/fcvt/test/test_fcvt_pipe.py
+++ src/ieee754/fcvt/test/test_fcvt_pipe.py
@@ -14,18 +14,22 @@ def fcvt_32(x):
 
 def test_down_pipe_fp32_16():
     dut = FPCVTDownMuxInOut(32, 16, 4)
-    runfp(dut, 32, "test_fcvt_down_pipe_fp32_16", Float32, fcvt_16, True)
+    runfp(dut, 32, "test_fcvt_down_pipe_fp32_16", Float32, fcvt_16, True,
+            n_vals=100)
 
 def test_down_pipe_fp64_16():
     dut = FPCVTDownMuxInOut(64, 16, 4)
-    runfp(dut, 64, "test_fcvt_down_pipe_fp64_16", Float64, fcvt_16, True)
+    runfp(dut, 64, "test_fcvt_down_pipe_fp64_16", Float64, fcvt_16, True,
+            n_vals=100)
 
 def test_down_pipe_fp64_32():
     dut = FPCVTDownMuxInOut(64, 32, 4)
-    runfp(dut, 64, "test_fcvt_down_pipe_fp64_32", Float64, fcvt_32, True)
+    runfp(dut, 64, "test_fcvt_down_pipe_fp64_32", Float64, fcvt_32, True,
+            n_vals=100)
 
 if __name__ == '__main__':
-    test_down_pipe_fp64_16()
-    test_down_pipe_fp32_16()
-    test_down_pipe_fp64_32()
+    for i in range(200):
+        test_down_pipe_fp64_16()
+        test_down_pipe_fp32_16()
+        test_down_pipe_fp64_32()
 
