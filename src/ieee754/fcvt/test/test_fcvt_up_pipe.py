507ff0dc73b0c537a87c3184d3af86b4044b9f9a
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2019-07-18 06:40:15 +0100

    change function names

--- src/ieee754/fcvt/test/test_fcvt_up_pipe.py
+++ src/ieee754/fcvt/test/test_fcvt_up_pipe.py
@@ -12,24 +12,24 @@ def fcvt_64(x):
 def fcvt_32(x):
     return Float32(x)
 
-def test_down_pipe_fp16_32():
+def test_up_pipe_fp16_32():
     dut = FPCVTUpMuxInOut(16, 32, 4)
-    runfp(dut, 16, "test_fcvt_down_pipe_fp16_32", Float16, fcvt_32, True,
+    runfp(dut, 16, "test_fcvt_up_pipe_fp16_32", Float16, fcvt_32, True,
           n_vals=100)
 
-def test_down_pipe_fp16_64():
+def test_up_pipe_fp16_64():
     dut = FPCVTUpMuxInOut(16, 64, 4)
-    runfp(dut, 16, "test_fcvt_down_pipe_fp16_64", Float16, fcvt_64, True,
+    runfp(dut, 16, "test_fcvt_up_pipe_fp16_64", Float16, fcvt_64, True,
           n_vals=100)
 
-def test_down_pipe_fp32_64():
+def test_up_pipe_fp32_64():
     dut = FPCVTUpMuxInOut(32, 64, 4)
-    runfp(dut, 32, "test_fcvt_down_pipe_fp32_64", Float32, fcvt_64, True,
+    runfp(dut, 32, "test_fcvt_up_pipe_fp32_64", Float32, fcvt_64, True,
           n_vals=100)
 
 if __name__ == '__main__':
     for i in range(200):
-        test_down_pipe_fp16_32()
-        test_down_pipe_fp16_64()
-        test_down_pipe_fp32_64()
+        test_up_pipe_fp16_32()
+        test_up_pipe_fp16_64()
+        test_up_pipe_fp32_64()
 
