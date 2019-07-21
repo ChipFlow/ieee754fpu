6352a29b6e1e73ff42a2172f60bfe825d33c3fac
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2019-07-21 10:33:37 +0100

    add bug cross-reference to #113 for FCVT unit tests

--- src/ieee754/fcvt/test/test_fcvt_pipe.py
+++ src/ieee754/fcvt/test/test_fcvt_pipe.py
@@ -13,16 +13,22 @@ def fcvt_32(x):
     return Float32(x)
 
 def test_down_pipe_fp32_16():
+    # XXX TODO: this has too great a dynamic range as input
+    # http://bugs.libre-riscv.org/show_bug.cgi?id=113
     dut = FPCVTDownMuxInOut(32, 16, 4)
     runfp(dut, 32, "test_fcvt_down_pipe_fp32_16", Float32, fcvt_16, True,
             n_vals=100)
 
 def test_down_pipe_fp64_16():
+    # XXX TODO: this has too great a dynamic range as input
+    # http://bugs.libre-riscv.org/show_bug.cgi?id=113
     dut = FPCVTDownMuxInOut(64, 16, 4)
     runfp(dut, 64, "test_fcvt_down_pipe_fp64_16", Float64, fcvt_16, True,
             n_vals=100)
 
 def test_down_pipe_fp64_32():
+    # XXX TODO: this has too great a dynamic range as input
+    # http://bugs.libre-riscv.org/show_bug.cgi?id=113
     dut = FPCVTDownMuxInOut(64, 32, 4)
     runfp(dut, 64, "test_fcvt_down_pipe_fp64_32", Float64, fcvt_32, True,
             n_vals=100)
